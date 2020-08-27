c8276c03e5aa7d986f41eaaa536a806133ed5978
iida nozomi <nozomi@mbp.local>
2020-08-28 08:18:39 +0900

    feature/authentication

--- client/App.tsx
+++ client/App.tsx
@@ -1,37 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
-import Apply from './src/components/templates/Apply';
-import Footer from './src/components/templates/Footer';
+import Apply from './src/components/pages/Apply';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import SignIn from './src/components/pages/SignIn';
+import SignUp from './src/components/pages/SignUp';
 // import StoryBook from './src/StoryBook'
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 export default function App() {
   const Tab = createBottomTabNavigator();
   return (
-    <View style={styles.container}>
-      <NavigationContainer>
-        <Tab.Navigator>
-          <Tab.Screen name='Home' component={Apply} />
-          <Tab.Screen name='Settings' component={SettingsScreen} />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </View>
+    <NavigationContainer >
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={SignUp} />
+        <Tab.Screen name='SignIn' component={SignIn} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 30,
+    paddingRight: 10,
+    paddingLeft: 10,
+    paddingTop: 30,
     height: '100%',
   },
 });
---
+++ client/src/components/pages/SignUp.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import {
+  Text,
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+
+type FormData = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+export default function SignUp() {
+  const { control, handleSubmit, errors } = useForm<FormData>();
+  const onSubmit = (data: FormData) => console.log(data);
+
+  return (
+    <View>
+      <Text style={{ textAlign: 'center', fontSize: 32, margin: 10 }}>
+        新規登録
+      </Text>
+      <Text>名前*</Text>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={value => onChange(value)}
+            value={value}
+          />
+        )}
+        name='username'
+        rules={{ required: true }}
+        defaultValue=''
+      />
+      <View style={styles.errContainer}>
+        {errors.username && (
+          <Text style={{ color: '#FF0000' }}>書き忘れています。</Text>
+        )}
+      </View>
+      <Text>メールアドレス*</Text>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={value => onChange(value)}
+            value={value}
+          />
+        )}
+        name='email'
+        rules={{ required: true }}
+        defaultValue=''
+      />
+      <View style={styles.errContainer}>
+        {errors.email && (
+          <Text style={{ color: '#FF0000' }}>書き忘れています。</Text>
+        )}
+      </View>
+
+      <Text>パスワード*</Text>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={value => onChange(value)}
+            value={value}
+          />
+        )}
+        name='password'
+        rules={{ required: true }}
+        defaultValue=''
+      />
+      <View style={styles.errContainer}>
+        {errors.password && (
+          <Text style={{ color: '#FF0000' }}>書き忘れています。</Text>
+        )}
+      </View>
+
+      <Button
+        title='ログイン'
+        onPress={handleSubmit(onSubmit)}
+        color='#f194ff'
+      />
+      <TouchableOpacity style={{ marginTop: 10 }}>
+        <Text>アカウントを既に持っていますか？</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  label: {
+    color: 'white',
+    margin: 20,
+    marginLeft: 0,
+  },
+  button: {
+    marginTop: 40,
+    color: 'white',
+    height: 40,
+    backgroundColor: '#ec5990',
+    borderRadius: 4,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 8,
+    backgroundColor: '#0e101c',
+  },
+  input: {
+    backgroundColor: 'white',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    height: 40,
+    padding: 10,
+    borderRadius: 4,
+  },
+  errContainer: {
+    height: 40,
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
+});
