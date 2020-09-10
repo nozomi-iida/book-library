fa38e737cccd59801bf76e27abc5cd791b6f68f3
iida nozomi <nozomi@mbp.local>
2020-09-10 12:13:52 +0900

    本の表紙を表示

--- client/src/components/pages/BookDetail.tsx
+++ client/src/components/pages/BookDetail.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useContext } from 'react';
-import { View, Text, StyleSheet, Button, Linking } from 'react-native';
+import { View, Text, StyleSheet, Button, Linking, Image } from 'react-native';
 import { RouteProp } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { IBook } from '../../types/book';
 import { AuthContext } from '../../stores/authStore';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/AntDesign';
+import asinMatcher from 'asin-matcher';
 
 type RootStackParamList = {
   apply: { book: IBook };
@@ -16,10 +17,13 @@ type RootStackParamList = {
 
 type ProfileScreenRouteProp = RouteProp<RootStackParamList, 'apply'>;
 
-type ScreeenNavigationProp = StackNavigationProp<RootStackParamList, 'edit' | 'permitForm' | 'readForm'>
+type ScreeenNavigationProp = StackNavigationProp<
+  RootStackParamList,
+  'edit' | 'permitForm' | 'readForm'
+>;
 
 interface Props {
-  navigation: ScreeenNavigationProp
+  navigation: ScreeenNavigationProp;
   route: ProfileScreenRouteProp;
 }
 
@@ -29,11 +33,18 @@ export default function BookDetail({ navigation, route }: Props) {
   const dateMonth = date.getMonth() + 1;
   const dateDate = date.getDate();
   const dateYear = date.getFullYear();
-  const {loginState} = useContext(AuthContext);
+  const { loginState } = useContext(AuthContext);
+  const asin = asinMatcher.getAsin(book.url);
   return (
     <View style={styles.container}>
       {book && (
         <View style={styles.card}>
+          <Image
+            source={{
+              uri: `http://images-jp.amazon.com/images/P//${asin}.09.LZZZZZZZ`,
+            }}
+            style={{width: 210, height: 270}}
+          />
           <Text
             style={{
               fontWeight: 'bold',
@@ -52,20 +63,36 @@ export default function BookDetail({ navigation, route }: Props) {
           <Text style={styles.text}>本の簡単な詳細: {book.description}</Text>
           <Text style={styles.text}>読みたい理由: {book.reason}</Text>
           {book.status === '許可' && (
-            <TouchableOpacity onPress={() => Linking.openURL(book.affiliateUrl)}><Text style={{marginBottom: 10,fontSize: 16}}>本を購入する<Icon name='arrowright' size={16} style={{ marginLeft: 10 }} /></Text></TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => Linking.openURL(book.affiliateUrl)}
+            >
+              <Text style={{ marginBottom: 10, fontSize: 16 }}>
+                本を購入する
+                <Icon name='arrowright' size={16} style={{ marginLeft: 10 }} />
+              </Text>
+            </TouchableOpacity>
           )}
           {loginState.username === book.username && (
             <>
               <View style={{ marginBottom: 10 }}>
-                <Button title='編集' onPress= {() => navigation.navigate('edit', {book: book})}  />
+                <Button
+                  title='編集'
+                  onPress={() => navigation.navigate('edit', { book: book })}
+                />
               </View>
             </>
           )}
           {book.status === '申請中' && (
-            <Button title='許可画面へ' onPress={() => navigation.navigate('permitForm', { book: book })} />
+            <Button
+              title='許可画面へ'
+              onPress={() => navigation.navigate('permitForm', { book: book })}
+            />
           )}
           {loginState.username === book.username && book.status === '許可' && (
-            <Button title='読了画面へ' onPress={() => navigation.navigate('readForm', { book: book })} />
+            <Button
+              title='読了画面へ'
+              onPress={() => navigation.navigate('readForm', { book: book })}
+            />
           )}
         </View>
       )}
@@ -83,9 +110,9 @@ const styles = StyleSheet.create({
     borderRadius: 6,
     elevation: 3,
     backgroundColor: '#fff',
-    shadowOffset: {width: 1, height: 1},
+    shadowOffset: { width: 1, height: 1 },
     shadowColor: '#333',
-    shadowOpacity: .3,
+    shadowOpacity: 0.3,
     shadowRadius: 2,
     marginHorizontal: 4,
     marginVertical: 4,
@@ -95,7 +122,7 @@ const styles = StyleSheet.create({
   },
   text: {
     marginBottom: 10,
-    fontSize: 18
+    fontSize: 18,
   },
   fixToText: {
     flexDirection: 'row',
